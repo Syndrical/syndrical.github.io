4b6912b59b07c8a51ff82768754176d848181a58
Syndrical <hoangk@myumanitoba.ca>
2020-05-03 23:34:59 -0500

    Updates

--- old/README.md
+++
@@ -1,2 +0,0 @@
-# syndrical.github.io
-Website
---
+++ precache-manifest.1618f75829fb9d3d50938f00a21e6cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f80c230da7b32ede7f573582bc3ae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e86664d333af44d54d31",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "715fdbe27ead656a5a47",
+    "url": "/static/js/2.e0d1cd81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e0d1cd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86664d333af44d54d31",
+    "url": "/static/js/main.686157a4.chunk.js"
+  },
+  {
+    "revision": "43989ecb2bac15c51acd",
+    "url": "/static/js/runtime-main.fcb110f7.js"
+  }
+]);
